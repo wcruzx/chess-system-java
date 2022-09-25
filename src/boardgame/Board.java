ed4242a51c5cec52a67c22bda238e7a6f433d00e
wcruzx <092.1307@alunos.unigrancapital.com.br>
2022-09-25 09:46:54 -0400

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -38,4 +38,10 @@ public class Board {
 	
 	}
 	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	
+	}
+	
 }

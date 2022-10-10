36aa763205ba4e6126403c41baf5d8740031406c
wcruzx <092.1307@alunos.unigrancapital.com.br>
2022-10-10 14:28:22 -0400

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -16,4 +16,9 @@ public class Rook extends ChessPiece {
 		
 	}
 	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
